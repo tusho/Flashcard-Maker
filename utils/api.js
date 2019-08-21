9f81da4cd1b52c87a210a8b914d2da304957f6b1
Armin Kuhestani <armin_kuhestani@apple.com>
2019-08-21 16:26:32 +1000

    Added api file for async storage of data

---
+++ utils/api.js
@@ -0,0 +1,17 @@
+import {AsyncStorage} from 'react-native'
+
+export function getDecks () {
+    //return all decks
+}
+
+export function getDeck () {
+    //return one deck with single id as argument
+}
+
+export function saveDeckTitle () {
+    //take title argument and save to deck
+}
+
+export function addCardToDeck () {
+    //take title and card argument and add the card to the list of questions for the deck with the associated title
+}

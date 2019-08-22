47e657324ca2e18de0cc8fc61a151e1c6097ec18
Armin Kuhestani <armin_kuhestani@apple.com>
2019-08-22 13:51:32 +1000

    Rendering basic deck info on Deck Overview page

--- components/DecksOverview.js
+++ components/DecksOverview.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, TouchableOpacity, StyleSheet, Platform, FlatList } from 'react-native'
 import { connect } from 'react-redux'
 import { getDecks } from '../utils/api'
 import { receiveDecks } from '../actions'
+
+const white = '#fff'
 
 class DecksOverview extends Component {
 
@@ -20,14 +22,54 @@ class DecksOverview extends Component {
 
     }
 
+
+
     render() {
+
+        const { decks } = this.props
+
+        let decksArray = Object.keys(decks).map((val) => {
+            return {'key': val, ...decks[val]}
+        })
+
         return (
             <View>
-                <Text>{JSON.stringify(this.props.decks)}</Text>
+                <FlatList
+                    data={decksArray}
+                    renderItem={({item}) =>
+                        <TouchableOpacity style={styles.item}>
+                            <Text>{JSON.stringify(item)}</Text>
+                        </TouchableOpacity>
+                    }
+                />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    item: {
+      backgroundColor: white,
+      borderRadius: Platform.OS === 'ios' ? 16 : 2,
+      padding: 20,
+      marginLeft: 10,
+      marginRight: 10,
+      marginTop: 17,
+      justifyContent: 'center',
+      shadowRadius: 3,
+      shadowOpacity: 0.8,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+    },
+    noDataText: {
+      fontSize: 20,
+      paddingTop: 20,
+      paddingBottom: 20
+    }
+})
 
 function mapStateToProps (decks) {
     return {

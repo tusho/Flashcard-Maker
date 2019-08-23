d20dc0bf7be483c0ea5c550eda1087bd331a96d5
Armin Kuhestani <armin_kuhestani@apple.com>
2019-08-23 14:09:01 +1000

    Added Tab Navigation

--- App.js
+++ App.js
@@ -1,18 +1,25 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Platform } from 'react-native'
 import DecksOverview from './components/DecksOverview'
 import AddDeck from './components/AddDeck'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import { Ionicons } from '@expo/vector-icons'
+import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
+const TabNavigator = createBottomTabNavigator({
+  'My Decks': DecksOverview,
+  'Add Deck': AddDeck,
+});
+
+const TabNav = createAppContainer(TabNavigator)
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
       <View style={{flex: 1}}>
-        <DecksOverview />
+        <TabNav />
       </View>
     </Provider>
   );

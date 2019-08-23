ae988b2a9ad3d5327871196a36e1d10612346eeb
Armin Kuhestani <armin_kuhestani@apple.com>
2019-08-23 14:54:11 +1000

    Added and styled status bar

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Platform } from 'react-native'
+import { View, Platform, StatusBar } from 'react-native'
 import DecksOverview from './components/DecksOverview'
 import AddDeck from './components/AddDeck'
 import { createStore } from 'redux'
@@ -7,6 +7,8 @@ import { Provider } from 'react-redux'
 import reducer from './reducers'
 import { Ionicons } from '@expo/vector-icons'
 import { createBottomTabNavigator, createMaterialTopTabNavigator, createAppContainer } from 'react-navigation';
+import Constants from 'expo-constants'
+import { lightGray } from './utils/colors'
 
 const TabNavigator = {
   'My Decks': {
@@ -21,14 +23,23 @@ const TabNavigator = {
       tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
     }
   },
-};
+}
 
 const TabNav = createAppContainer(Platform.OS === 'ios' ? createBottomTabNavigator(TabNavigator) : createMaterialTopTabNavigator(TabNavigator))
+
+function MyStatusBar ({backgroundColor, ...props}) {
+  return (
+    <View style={{ backgroundColor, height: Constants.statusBarHeight }}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props} />
+    </View>
+  )
+}
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
       <View style={{flex: 1}}>
+        <MyStatusBar backgroundColor={lightGray} barStyle="light-content" />
         <TabNav />
       </View>
     </Provider>
